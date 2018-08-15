93d8131eee36674376cab8137d98e40c4df3ccdc
Anner de Jong <anner.de.jong@outlook.com>
2018-08-15 18:43:55 +0900

    added RMSE based on previous project

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,41 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(5);
+  rmse << 0,0,0,0,0;
+
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+
+  if (estimations.size() != ground_truth.size()) {
+    cout << "CalculateRMSE() - Error - estimation size != ground truth size" << endl;
+    return rmse;
+  }
+  // assuming all entries in either array have the same number of elements
+  // thus we only need to check if the first entries of both arrays have equal size:
+  float en = estimations[0].size(); // number of elements (n) in current (i) estimation
+  float gn = ground_truth[0].size(); // number of elements (n) in current (i) ground truth
+
+  if (en != gn) {
+    cout << "CalculateRMSE() - Error - estimation[0] size != ground truth[0] size" << endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  for(int i=0; i < estimations.size(); ++i){
+
+    // from answer, one less for-loop
+    VectorXd difference = estimations[i] - ground_truth[i];
+    VectorXd residual   = difference.array().square();
+    rmse += residual;
+  }
+
+  //calculate the mean
+  rmse /= estimations.size();
+
+  //calculate the squared root
+  rmse  = rmse.array().sqrt();
+
+  return rmse;
+
 }
